31b17d8f7089962110a61a3a5a923f423f9336d3
rayleigh420 <nhatduy0409@gmail.com>
2023-10-06 21:19:21 +0700

    Test param and query route in nestjs

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Request } from 'express';
 
 @Controller('user')
 export class UserControler {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getUser(): string {
-    return this.userService.getUser();
+  getUser(@Query('id') id: string, @Req() req: Request): string {
+    return 'Get detail user by query string id: ' + id + ' or ' + req.query.id;
+    // return this.userService.getUser();
+  }
+
+  @Get('/:id')
+  getDetailUserById(@Param('id') id: string, @Req() req: Request): string {
+    return 'Get detail user by id ' + id + ' or ' + req.params.id;
+  }
+
+  @Get('')
+  getDetailUserByQueryId(@Query('id') id: string, @Req() req: Request): string {
+    return 'Get detail user by query string id: ' + id + ' or ' + req.query.id;
   }
 }
